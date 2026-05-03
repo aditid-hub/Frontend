5856418a8e0c6d464c9d1d52f932dc3bdd4dca0f
unknown <aditid155@gmail.com>
2026-05-03 16:31:53 +0530

    Aditi

---
+++ JAVASCRIPT/DAY_1.js
@@ -0,0 +1,32 @@
+console.log("Hello! Aditi Dubey here, my first js");
+console.log("I am learning JavaScript and it's really fun!");   
+naam = "Bageshwar Dham"
+kakcha = "satvi fail"
+console.log(naam);
+console.log(kakcha)
+x= null;
+y= undefined;
+console.log(x);
+console.log(y);
+isfollow= "true";
+console.log(isfollow);
+{
+    let a= 5;
+    a= 20
+    console.log(a);
+}
+{
+    let a= 10;
+    console.log(a);
+}
+const Aditi = {
+    full_Name = "Aditi Dubey",
+    age = 20,
+    cgpa = 9, 
+    isPass = true
+};
+console.log(Aditi.age);
+// or
+console.log(Aditi["age"]);
+console.log(typeof Aditi["age"]); 
+
---
+++ JAVASCRIPT/Day_2.js
@@ -0,0 +1,15 @@
+let a=5;
+let b=10;
+let sum = a+b;
+console.log(sum);
+let sub = a-b;
+console.log(sub);
+let mul = a*b;
+console.log(mul);
+let div = a/b;
+console.log(div);
+console.log(a**2);
+//unary operator 
+let c= 10;
+console.log(c);
+c++;
